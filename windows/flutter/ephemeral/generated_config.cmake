e53c390b2adc879c8f513e158f05bc65830c7980
dxmarquez <113218066+dxmarquez@users.noreply.github.com>
2022-11-08 08:32:17 -0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main"
+  "FLUTTER_TARGET=C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\default.DESKTOP-G8IHFOF\\Desktop\\f_currency_converter_template-main\\f_currency_converter_template-main\\.dart_tool\\package_config.json"
+)
